756f0767a9351226d2eb95e1df64843cac2e80ae
Andrew Kolb <akolb87@gmail.com>
2018-11-12 21:25:12 -0600

    re-org flow

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 'use strict';
 
 // const searchURL = 'https://pokeapi.salestock.net/api/v2/pokemon/';
-// const searchURL = "https://pokeapi.co/api/v2/pokemon/";
+// const searchURL = 'https://pokeapi.co/api/v2/pokemon/';
 const searchURL = 'https://pokeapi-215911.firebaseapp.com/api/v2/pokemon/';
 
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchTerm = $('#js-search-term').val().toLowerCase();
+        getPokemon(searchTerm);
+    });
+}
+
+function getPokemon(query) {
+    const url = searchURL + query;
+
+    console.log(url);
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            console.log(err);
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+}
 
 function displayResults(responseJson) {
     console.log(responseJson);
@@ -46,33 +72,6 @@ function displayAbilities(responseJson) {
     for (let i = 0; i < responseJson.moves.length; i++) {
         $('#pokemon-abilities').append(`<li>${responseJson.moves[i].move.name}</li>`);
     };
-}
-
-function getPokemon(query) {
-    const url = searchURL + query;
-
-    console.log(url);
-
-    fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => displayResults(responseJson))
-        .catch(err => {
-            console.log(err);
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
-}
-
-function watchForm() {
-    $('form').submit(event => {
-        event.preventDefault();
-        const searchTerm = $('#js-search-term').val().toLowerCase();
-        getPokemon(searchTerm);
-    });
 }
 
 $(watchForm);
